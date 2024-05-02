9593f3dcee85c06fb3a8fd6ee5c80de4b262eff3
russokun <agustin.russo2323@gmail.com>
2024-05-01 21:39:07 -0400

    se añade clientControler para gestion de las peticiones HTTP.

---
+++ src/main/java/com/mindhub/homebanking/controlers/ClientControler.java
@@ -0,0 +1,27 @@
+package com.mindhub.homebanking.controlers;
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClientControler {
+
+  @Autowired
+  ClientRepository clientRepository;
+
+  @GetMapping("/hello")
+  public String helloClients() {
+    return "Hello World from API!";
+  }
+
+  @GetMapping("/")
+  public List <Client> clientsList() {
+    return clientRepository.findAll();
+  }
+}
